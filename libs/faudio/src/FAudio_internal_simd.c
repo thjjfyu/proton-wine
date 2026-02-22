15e09cc43d30aae2b7d0d942eb0ba8b9df63d93f
Neiton <zekaeeee180@gmail.com>
2026-02-22 19:51:12 +0300

    arm64ec: disable SSE2 path in FAudio SIMD

--- libs/faudio/src/FAudio_internal_simd.c
+++ libs/faudio/src/FAudio_internal_simd.c
@@ -32,7 +32,7 @@
  * https://hg.icculus.org/icculus/mojoAL/file/default/mojoal.c
  */
 
-#if defined(__x86_64__) || defined(_M_X64)
+#if (defined(__x86_64__) || defined(_M_X64)) && !defined(__arm64ec__)
 	/* Some platforms fail to define this... */
 	#ifndef __SSE2__
 	#define __SSE2__ 1
@@ -40,7 +40,7 @@
 
 	/* x86_64 guarantees SSE2. */
 	#define NEED_SCALAR_CONVERTER_FALLBACKS 0
-#elif defined(__aarch64__) || defined(_M_ARM64)
+#elif defined(__aarch64__) || defined(_M_ARM64) || defined(__arm64ec__)
 	/* Some platforms fail to define this... */
 	#ifndef __ARM_NEON__
 	#define __ARM_NEON__ 1
@@ -62,13 +62,13 @@
 #endif
 
 /* Our NEON paths require AArch64, don't check __ARM_NEON__ here */
-#if defined(__aarch64__) || defined(_M_ARM64)
+#if defined(__aarch64__) || defined(_M_ARM64) || defined(__arm64ec__)
 #include <arm_neon.h>
 #define HAVE_NEON_INTRINSICS 1
 #endif
 
 
-#ifdef __SSE2__
+#if defined(__SSE2__) && !defined(__arm64ec__)
 #include <emmintrin.h>
 #define HAVE_SSE2_INTRINSICS 1
 #endif
